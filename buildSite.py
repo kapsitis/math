90d386bdabe6abb9c563292ce1340a30090fe9e1
Kalvis Apsitis <kalvis.apsitis@gmail.com>
2019-05-15 20:26:59 +0300

    buildScript

---
+++ buildSite.py
@@ -0,0 +1,30 @@
+import os
+import shutil
+import subprocess
+
+
+def copyDirectory(src, dest):
+    try:
+        shutil.copytree(src, dest)
+    # Directories are the same
+    except shutil.Error as e:
+        print('Directory not copied. Error: %s' % e)
+    # Any error saying that the directory doesn't exist
+    except OSError as e:
+        print('Directory not copied. Error: %s' % e)
+
+resTypes = ['exam', 'prob', 'tale']
+
+for resType in resTypes:
+    ROOT = 'src/site/%s' % resType
+    themePath = '%s/themes/ddgatve' % ROOT
+    subDirectories = set(next(os.walk(ROOT))[1]).difference(set(['themes']))
+    for dd in subDirectories:
+        print("Processing with landslide %s" % dd)
+        srcPath = '%s/%s/content.md' % (ROOT,dd)
+        destPath = '%s/%s/content.html' % (ROOT,dd)
+        subprocess.call(['landslide','--relative',srcPath,'-t',themePath,'-d',destPath])
+        copyDirectory('%s/%s' % (ROOT,dd), 'target/%s/%s' % (resType,dd))
+
+
+
