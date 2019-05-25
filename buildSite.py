b0d4bca8582eac88201e4dccc0f13ed78b30ea0c
Kalvis Apsitis <kalvis.apsitis@gmail.com>
2019-05-25 23:49:56 +0300

    fixed script again

--- buildSite.py
+++ buildSite.py
@@ -28,7 +28,7 @@ for resType in resTypes:
        	subprocess.call(['pandoc','-t','revealjs','-s',
 		'-o','content.html','content.md','--slide-level=2',
 		'-V','revealjs-url=../../reveal.js','--metadata', 'pagetitle="Uzdevumi"',
-    		'--mathjax="https://cdn.mathjax.org/mathjax/latest/MathJax.js?config=TeX-AMS-MML_HTMLorMML"',
+    		'--mathjax=https://cdn.mathjax.org/mathjax/latest/MathJax.js?config=TeX-AMS-MML_HTMLorMML',
 		'-V','theme=white'], cwd=workingDir)
         copyDirectory('%s/%s' % (ROOT,dd), 'target/%s/%s' % (resType,dd))
 copyDirectory('src/site/reveal.js', 'target/reveal.js')
