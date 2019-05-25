230630c75131a80dd99db55254fc96ef1ce537b9
Kalvis Apsitis <kalvis.apsitis@gmail.com>
2019-05-25 23:35:37 +0300

    one more fix

--- buildSite.py
+++ buildSite.py
@@ -27,7 +27,7 @@ for resType in resTypes:
         # subprocess.call(['landslide','--relative',srcPath,'-t',themePath,'-d',destPath])
        	subprocess.call(['pandoc','-t','revealjs','-s',
 		'-o','content.html','content.md','--slide-level=2',
-		'-V','revealjs-url=../../reveal.js','--metadata pagetitle="Uzdevumi"',
+		'-V','revealjs-url=../../reveal.js','--metadata', 'pagetitle="Uzdevumi"',
     		'--mathjax="https://cdn.mathjax.org/mathjax/latest/MathJax.js?config=TeX-AMS-MML_HTMLorMML"',
 		'-V','theme="white"'], cwd=workingDir)
         copyDirectory('%s/%s' % (ROOT,dd), 'target/%s/%s' % (resType,dd))
