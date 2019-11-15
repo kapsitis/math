4184193b464081cd91debd15d1f43833d70e146c
kapsitis <kalvis.apsitis@gmail.com>
2019-11-16 00:59:23 +0200

    SOmething

--- src/math-build/src/main/scala/lv/ddgatve/math/build/BuildSite.scala
+++ src/math-build/src/main/scala/lv/ddgatve/math/build/BuildSite.scala
@@ -7,10 +7,14 @@ import org.apache.commons.io.filefilter.TrueFileFilter
 import scala.collection.JavaConversions._
 import scala.collection.mutable.ListBuffer
 
+import sys.process._
+
 object BuildSite {
   
   def copyDirectory(src:String, dest:String): Unit = {
-    FileUtils.copyDirectory(new File(src), new File(dest))
+    println("src is " + src)
+    println("dest is " + dest)
+    //FileUtils.copyDirectory(new File(src), new File(dest))
   }
    
   def rmDirectory(src:String): Unit = {
@@ -35,33 +39,33 @@ object BuildSite {
     } else {
        "../../../../workspace/linen-tracer-682"
     }
-    println("osName is " + osName)
-    println("DestRoot is " + destRoot)
-    
+
     for (resType <- resTypes) {
-      println(s"Destination for presentations - $destRoot/$resType-tales")
-      val srcRoot = new File(s"../../src/site/$resType")
-      println("file is " + srcRoot)
-      val subDirectories = FileUtils.listFilesAndDirs(srcRoot,
-          TrueFileFilter.INSTANCE, TrueFileFilter.INSTANCE)
-      
-      val subDirIter = subDirectories.iterator()
-      var subDirList = new ListBuffer[File]()
-      while (subDirIter.hasNext()) {
-        val ff = subDirIter.next()
-        if (ff.isDirectory()) {
-          subDirList += ff
-          println("ff is " + ff)
-        }
-      }
-      
-      println("subDirList is " + subDirList.size)
-          
-      //for (dd <- subDirectories) {
-      //  println(s"Processing with pandoc/reveal, dir=$dd")
-      //}
-    }
-    
-  }
+      //println(s"Destination for presentations - $destRoot/$resType-tales")
+      val srcRoot = s"../../src/site/$resType"
+      val srcRootFile = new File(srcRoot)
+      //println("file is " + srcRoot)
   
+      val subDirList = srcRootFile.listFiles.filter(_.isDirectory).toList
+      val subPresentations = subDirList.filter(p => !skip_directories.contains(p.getName))
+      
+      //println("# of presentations: " + subPresentations.size)
+          
+      for (dd <- subPresentations) {
+        if (resType == "rbs" && dd.getName() == "youtube-data-part1") {
+           println(s"Processing with pandoc/reveal, dir=$dd")
+           copyDirectory(dd.getAbsolutePath, destRoot + "/" + resType + "-tales/" + dd.getName)
+        }
+        
+//        val out = Process(Seq(
+//            "pandoc","-t","revealjs","-s",
+//            "-o","content.html","content.md","--slide-level=2",
+//            "-V","revealjs-url=../../reveal.js","--metadata", "pagetitle=\"Uzdevumi\"",
+//            "--mathjax=https://cdn.mathjax.org/mathjax/latest/MathJax.js?config=TeX-AMS-MML_HTMLorMML",
+//            "-V", "theme=white"), dd).!!
+//        println("out = " + out)
+        
+      }
+    }
+  }
 }
