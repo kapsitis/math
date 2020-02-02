7a06a0443b55ed28aaff45344e06ffbd572eb92d
Kalvis Apsitis <kalvis.apsitis@gmail.com>
2020-02-03 01:48:59 +0200

    New sample quiz

---
+++ src/site/discrete/coq-numbertheory/arithmetic_theorems.v
@@ -0,0 +1,104 @@
+(** #<a href="../../discrete/assignments.html">Back to Discrete Assignments</a># *)
+
+(** * Arithmetic Theorems from the Rosen book *)
+
+Require Import ZArith.
+Open Scope Z_scope.
+
+
+
+(** ** Transitivity of Division *)
+
+(** Theorem 1.i from 4.1 (Rosen2019, p.252). *)
+
+Theorem divide_sum: forall a b c, (a | b) -> (a | c) -> (a | b+c).
+Proof. 
+  intros a b c. 
+  unfold Z.divide.
+  intros H1 H2. 
+  destruct H1. 
+  destruct H2.
+  exists (x+x0).
+  rewrite H.
+  rewrite H0.
+  ring.
+Qed.
+
+
+(** Theorem 1.ii from 4.1 (Rosen2019, p.252). *)
+
+Theorem divide_multiple: forall a b c, (a | b) -> (a | b*c).
+Proof. 
+  intros a b c.
+  unfold Z.divide.
+  intros H1.
+  destruct H1.
+  exists (x*c).
+  rewrite H.
+  ring.
+Qed.
+
+(** This is strange that we need this axiom despite commutativity *)
+Theorem divide_multiple_left: forall a b c, (a | b) -> (a | c*b).
+Proof. 
+  intros a b c.
+  unfold Z.divide.
+  intros H1.
+  destruct H1.
+  exists (x*c).
+  rewrite H.
+  ring.
+Qed.
+
+
+
+
+
+(** ** Transitivity of Division *)
+
+(** #<b>Theorem:</b># Let n,m,p be 
+any integers. If n divides m 
+and m divides p, then n divides p. 
+See (Rosen2019, p.252), Theorem 1.iii *)
+
+(** The Coq proof is quoted from (Knepley2019, p.111) *)
+
+Theorem divide_trans : forall a b c, (a | b) -> (b | c) -> (a | c).
+Proof.
+  intros a b c.
+  unfold Z.divide.
+  intros H1 H2.
+  destruct H1.
+  destruct H2.
+  exists (x0*x).
+  rewrite H0.
+  rewrite H.
+  ring.
+Qed.
+
+
+Corollary divide_lincombination: 
+    forall a b c m n, (a | b) /\ (a | c) -> (a | m*b + n*c).
+Proof. 
+  intros a b c m n.
+  intros H. 
+  destruct H as [H1 H2].
+  pose (divide_multiple_left a b m) as H3.
+  apply H3 in H1 as H4.
+  pose (divide_multiple_left a c n) as H5.
+  apply H5 in H2 as H6.
+  clear H3 H5.
+  pose (divide_sum a (m*b) (n*c)) as H7.
+  pose (H7 H4) as H8.
+  pose (H8 H6) as H9.
+  unfold Z.divide. 
+  unfold Z.divide in H9.
+  destruct H9. 
+  exists (x).
+  exact H.
+Qed.
+
+
+ 
+
+
---
+++ src/site/discrete/coq-numbertheory/arithmetic_theorems_nat.v
@@ -0,0 +1,30 @@
+Require Import Arith.
+
+(*
+Inductive even : nat -> Prop :=
+  even0 : even 0
+  | evenS : forall x:nat, even x -> even (S (S x)).
+*)
+
+Inductive odd: nat -> Prop :=
+  odd1 : odd (S 0)
+  | oddS : forall x:nat, odd x -> odd (S (S x)).
+
+(** This is Chapter 1.7 from the textbook (Rosen2019, p.87) *)
+
+Theorem example1: forall n: nat, (odd n) -> (odd (n*n)).
+Proof. 
+  induction n.
+
+
+Inductive even : nat -> Prop :=
+| ev_0 : even 0
+| ev_SS (n : nat) (H : even n) : even (S (S n)).
+
+Eval compute in (even 1).
+
+Fail Inductive wrong_ev (n : nat) : Prop :=
+  | wrong_ev_0 : wrong_ev 0
+  | wrong_ev_SS : wrong_ev n  wrong_ev (S (S n)).
+(* ===> Error: Last occurrence of "wrong_ev" must have "n"
+        as 1st argument in "wrong_ev 0". *)
