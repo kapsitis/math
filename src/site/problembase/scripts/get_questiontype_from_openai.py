765aad7ef3a02e016b7d548833e5850d142ad6ce
Elizabete Ozolina <etebalize@gmail.com>
2024-05-07 20:25:44 +0300

    added new scripts

---
+++ src/site/problembase/scripts/get_questiontype_from_openai.py
@@ -0,0 +1,67 @@
+from openai import OpenAI
+client = OpenAI()
+import json
+
+def classify_math_problem(problem_text,prompt_name):
+    # Classify the type of math problem using OpenAI
+    prompt0 = f"Lūdzu atrodi matemātikas uzdevuma tipu: \n\n ```{problem_text}```\n\n" \
+             " Iespējamie jautājumu tipi ir: " \
+             "'FindAll' (uzdevumi, kuros jāatrod visi atrisinājumi); " \
+             "'FindCount' (uzdevumi, kuros jāsaskaita cik iespēju vai atrisinājumu skaits); " \
+             "'FindOptimal' (uzdevumi, kuros jāatrod maksimālais vai minimālais risinājums); " \
+             "'FindExample' (uzdevumi, kuros jāatrod 1 piemērs vai pretpiemērs); " \
+             "'Prove' (uzdevumi, kuros jāpierāda apgalvojums); " \
+             "'ProveDisprove' (uzdevumi, kuros apgalvojums ir jāpierāda vai jāapgāž); " \
+             "'Algorithm' (uzdevumi, kuros jāatrod procedūra vai spēles stratēģija)."
+    
+    prompt1  = "Imagine that you are IMO problem selection committee member - and you are selecting problems for the International Math Olympiad." \
+"Your task is to find the most appropriate topic for each problem we specify - whether it is Algebra, Combinatorics, Geometry or Number Theory." \
+             "Geometry: Geometry problems in math olympiads often involve creative problem-solving using principles " \
+             "of Euclidean geometry. These problems may require the use of geometric properties, theorems, " \
+             "and constructions to prove statements or find unknown quantities." \
+             "Algebra: Algebra problems typically involve the manipulation and analysis of equations, inequalities, " \
+             "and algebraic" \
+            "expressions. These problems may require techniques such as factorization, substitution, manipulation " \
+             "of " \
+              "polynomials, and the application of algebraic identities." \
+             "Number Theory: Number theory problems focus on properties and relationships of integers." \
+             "These problems often involve divisibility, prime numbers, modular arithmetic, and properties of " \
+             "sequences and series." \
+             "Combinatorics: Combinatorics problems deal with counting, arranging, and selecting objects. These " \
+             "problems may " \
+             "involve permutations, combinations, pigeonhole principle, graph theory, and other counting principles." \
+             f"Here is a problem you would need to classify: \n\n```{problem_text}```\n\n"
+    
+    all_prompts = {'questionType_LV': prompt0, 'domain_EN': prompt1}
+
+    response = client.chat.completions.create(
+      model="gpt-4-turbo",
+      response_format={ "type": "json_object" },
+      messages=[
+        {"role": "system", "content": "You are a helpful assistant designed to output JSON. JSON should contain one property named 'uzdevuma_tips'"},
+        {"role": "user", "content": all_prompts[prompt_name]}
+      ]
+    )
+    print("**********************************")
+    data = json.loads(response.choices[0].message.content)
+    questiontype_text = data["uzdevuma_tips"]
+    print(questiontype_text)
+    print("**********************************")
+    return questiontype_text
+
+
+def main():
+
+    # User input
+    problem_text = """Uz tāfeles pa reizei uzrakstīti visi naturālie skaitļi no $1$ līdz $n$ ieskaitot. 
+Ar vienu gājienu var izvēlēties divus uz tāfeles uzrakstītus skaitļus 
+(apzīmēsim tos ar $a$ un $b$), nodzēst tos un to vietā uzrakstīt $\left| a^2-b^2 \right|$. 
+Pēc $n-1$ gājiena uz tāfeles paliek viens skaitlis.  
+Vai tas var būt $0$, ja **(a)** $n=8$, **(b)** $n=9$?"""
+
+    # Classify the problem
+    classify_math_problem(problem_text)
+
+
+if __name__ == "__main__":
+    main()
---
+++ src/site/problembase/scripts/openai_questiontype.py
@@ -0,0 +1,78 @@
+import re
+import sys
+sys.path.append('.')
+from get_questiontype_from_openai import *
+
+
+# Read problems one by one from Markdown file "filepath"
+def extract_sections_from_md(filepath):
+    section_titles = []
+    current_section = None
+    sections = []
+    title = "NA" 
+
+    heading_re = re.compile(r'^#\s+<lo-sample/>\s+(.*)')
+
+    with open(filepath, 'r', encoding='utf-8') as file:
+        for line in file:
+            m = heading_re.match(line)
+            if m:
+                new_title = m.group(1)
+                # append the previous (title, current_section)
+                if current_section is not None:
+                    sections.append((title, current_section))
+                title = new_title
+                current_section = ''
+            elif current_section is not None:
+                # before seeing the first title, we do not append anything
+                current_section += line
+    # Append the last (title, current_section)
+    if current_section:
+        sections.append((title, current_section))
+    return sections
+
+def normalize_text(text):
+    meta_start = text.find('<small>')
+    if meta_start > 0:
+        # meta_end = text.find('</small>')
+        text = text[0:meta_start] # Nomet uzdevumam metainformāciju (un atrisinājumu, ja tāds tur ir)
+        # text = text+'\n\n'+text[meta_end:]
+    return text
+
+# def category_number(arg):
+#     categories = {'Find.All':0, 'Find.Count':1, 'Find.Optimal':2, 'Find.Example':3, 'Prove':4, 'ProveDisprove':5, 'Algorithm':6}
+#     if arg in categories:
+#         return categories[arg]
+#     else:
+#         return -1
+
+if __name__ == '__main__':
+    categories = ["Find.All", "Find.Count", "Find.Optimal", "Find.Example", 
+                  "Prove", "ProveDisprove", "Algorithm"]
+
+    questionType_re = re.compile(r'.+?questionType:([\w\.,]+).+?',re.DOTALL)
+    problemList = extract_sections_from_md(f'../{sys.argv[1]}/content.md')
+
+    # prepare training data: problem_data_set un category_data_set
+    problem_data_set = []
+    category_data_set = []
+    title_data_set = []
+    for (title, problem) in problemList:
+        category = 'NA'
+        m = questionType_re.match(problem)  # Mēģina atrast questionType
+        if m:
+            category = m.group(1)
+        problem = normalize_text(problem)
+        
+        answer = classify_math_problem(problem,'questionType_LV') # problem un prompt nosaukums
+        with open("questiontype_openai.csv", 'a') as file1:
+            file1.write(f'{title},"{category}","{answer}"\n')
+
+
+        if category != 'NA':
+            problem_data_set.append(problem)
+            category_data_set.append(category)
+            title_data_set.append(title)
+
+        print(problem)
+        print()
